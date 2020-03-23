f15220cd631b957d13196d6563dfd43cd8055b12
diversemix <diversemix@gmail.com>
2020-03-23 14:24:24 +0000

    getting there...

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const chop = (numberToFind, arrayToSearch) => {
+    if (!Array.isArray(arrayToSearch)) {
+        throw new Error('This is not an array');
+    }
+
+    const len = arrayToSearch.length;
+    if (len === 0) {
+        return -1;
+    }
+
+    let lower_bound = 0;
+    let upper_bound = len - 1;
+    let found = false;
+    while (!found && lower_bound !== upper_bound) {
+        if (arrayToSearch[lower_bound] == numberToFind) {
+            return lower_bound;
+        }
+    }
     return 0; // index of the found number in the array
 }
 
--- index.test.js
+++ index.test.js
@@ -1,7 +1,37 @@
 const { chop } = require('./index');
 
 describe('Test of chop', () => {
+    it('throws if not an array', () => {
+        expect(() => chop(3, 'chickens')).toThrow('This is not an array');
+    })
+
+    it('does not throw if an array', () => {
+        expect(() => chop(3, [])).not.toThrow('This is not an array');
+    })
+
+    it('returns -1 when empty array', () => {
+        expect(chop(3, [])).toBe(-1);
+    })
+
     it('works', () => {
-        expect(chop(0, 5)).toBe(0);
+        expect(chop(3, [])).toBe(-1);
+        expect(chop(3, [1])).toBe(-1);
+        expect(chop(1, [1])).toBe(0);
+        expect(chop(1, [1, 3, 5])).toBe(0);
+        expect(chop(3, [1, 3, 5])).toBe(1);
+        expect(chop(5, [1, 3, 5])).toBe(2);
+        expect(chop(0, [1, 3, 5])).toBe(-1);
+        expect(chop(2, [1, 3, 5])).toBe(-1);
+        expect(chop(4, [1, 3, 5])).toBe(-1);
+        expect(chop(6, [1, 3, 5])).toBe(-1);
+        expect(chop(1, [1, 3, 5, 7])).toBe(0);
+        expect(chop(3, [1, 3, 5, 7])).toBe(1);
+        expect(chop(5, [1, 3, 5, 7])).toBe(2);
+        expect(chop(7, [1, 3, 5, 7])).toBe(3);
+        expect(chop(0, [1, 3, 5, 7])).toBe(-1);
+        expect(chop(2, [1, 3, 5, 7])).toBe(-1);
+        expect(chop(4, [1, 3, 5, 7])).toBe(-1);
+        expect(chop(6, [1, 3, 5, 7])).toBe(-1);
+        expect(chop(8, [1, 3, 5, 7])).toBe(-1);
     })
 });
